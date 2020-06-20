902b5e534418b9ca6e170f3686f88762a010cadb
Piyush <piyush.bisht3@gmail.com>
2020-06-20 23:42:02 +0530

    Updates

---
+++ precache-manifest.2d1f3e94e0bbe1a2a3f6cdc64a51730f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4bdaa48112c81f539eef115acda9cb3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "56bb3422241e9382ada0",
+    "url": "/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "08621f7a6dbdad27b7b4",
+    "url": "/static/js/2.9a982912.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.9a982912.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "56bb3422241e9382ada0",
+    "url": "/static/js/main.92a3c971.chunk.js"
+  },
+  {
+    "revision": "57b0d65c7bc9d5d7aeff",
+    "url": "/static/js/runtime-main.58c5245d.js"
+  },
+  {
+    "revision": "61c188ccdd9ef9216c64da8e6a27dae0",
+    "url": "/static/media/spinner.61c188cc.gif"
+  }
+]);
